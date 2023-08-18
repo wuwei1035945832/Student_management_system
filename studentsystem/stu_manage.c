a58afe6cad7d021c3600f7d087df9b932977b6de
wuwei1035945832 <140374426+wuwei1035945832@users.noreply.github.com>
2023-08-18 09:18:27 +0800

    修改版本
    用链表修改了程序

--- studentsystem/stu_manage.c
+++ studentsystem/stu_manage.c
@@ -7,89 +7,108 @@ typedef struct stu
 	int num;
 	float score[3];
 	float ave;
-}STU;
+	struct stu *next;
+} STU;
 
-STU st[N];
+STU *H = NULL; // 全局链表头
 static int n;
 
 void help();
-void type();
-void list();
-void average();
-void sort();
+void type(STU *head);
+void list(STU *head);
+void average(STU *head);
+void sort(STU *head);
 void search();
 void add();
 void del();
 void modify();
 
+STU *LLCreate()
+{
+	STU *head = (STU *)malloc(sizeof(STU));
+	if (head == NULL)
+	{
+		printf("LLCreate 失败，分配内存失败。\n");
+		exit(1);
+	}
+	head->next = NULL;
+	return head;
+}
+
 int main(int argc, const char *argv[])
 {
 	char order;
-	
+	H = LLCreate(); // 使用全局变量
 	help();
-	while(1)
+	while (1)
 	{
 		printf("请输入命令=");
 		scanf("%c", &order);
-		switch(order)
+		switch (order)
 		{
-			case 'H':
-				help();
-				break;
-			case 'T':
-				//if(n == 0)
-				//	type();
-				//else
-					add();
-				break;
-			case 'A':
-				if(n == 0)
-					puts("成绩表为空！请先使用命令T录入学生成绩。");
-				else
-					average();
-				break;
-			case 'L':
-				if(n == 0)
-					puts("成绩表为空！请先使用命令T录入学生成绩。");
-				else
-					list();
-				break;
-			case 'P':
-				if(n == 0)
-					puts("成绩表为空！请先使用命令T录入学生成绩。");
-				else
-					sort();
-				break;
-			case 'S':
-				if(n == 0)
-					puts("成绩表为空！请先使用命令T录入学生成绩。");
-				else
-					search();
-				break;
-			case 'C':
-				system("clear");
-				break;
-			case 'D':
-				if(n == 0)
-					puts("成绩表为空！请先使用命令T录入学生成绩。");
-				else
-					del();
-				break;
-			case 'X':
-				if(n == 0)
-					puts("成绩表为空！请先使用命令T录入学生成绩。");
-				else
-					modify();
-				break;				
-			case 'Q':
-				exit(1);
-			
+		case 'H':
+			help();
+			break;
+		case 'T':
+			//if(n == 0)
+			//	type();
+			//else
+			add();
+			break;
+		case 'A':
+			if (n == 0)
+				puts("成绩表为空！请先使用命令T录入学生成绩。");
+			else
+				average(H); // 传入链表头指针作为参数
+			break;
+
+		case 'L':
+			if (n == 0)
+				puts("成绩表为空！请先使用命令T录入学生成绩。");
+			else
+				list(H);
+			break;
+		case 'P':
+			if (n == 0)
+				puts("成绩表为空！请先使用命令T录入学生成绩。");
+			else
+				sort(H);
+			break;
+		case 'S':
+			if (n == 0)
+				puts("成绩表为空！请先使用命令T录入学生成绩。");
+			else
+				search();
+			break;
+		case 'C':
+			system("clear");
+			break;
+		case 'D':
+			if (n == 0)
+				puts("成绩表为空！请先使用命令T录入学生成绩。");
+			else
+				del(H);
+			break;
+		case 'X':
+			if (n == 0)
+				puts("成绩表为空！请先使用命令T录入学生成绩。");
+			else
+				modify();
+			break;
+		case 'E':
+			if (n == 0)
+				puts("成绩表为空！请先使用命令T录入学生成绩。");
+			else
+				add();
+			break;
+		case 'Q':
+			exit(1);
 		}
-		getchar();//用scanf输入字符时，会同时把回车接受到，放在
-				//缓存区中，执行下次while循环时，会打印两次输出语句，
-				//所以使用getcahr来接收垃圾字符	
+		getchar(); //用scanf输入字符时，会同时把回车接受到，放在
+				   //缓存区中，执行下次while循环时，会打印两次输出语句，
+				   //所以使用getcahr来接收垃圾字符
 	}
-	
+
 	return 0;
 }
 
@@ -106,190 +125,250 @@ void help()
 	printf("*        P = 按平均成绩由高到低排序  *\n");
 	printf("*        S = 按学号查询学生成绩      *\n");
 	printf("*        C = 清屏                    *\n");
-	printf("*        Q = 退出系统                *\n");
 	printf("*        D = 删除学生                *\n");
 	printf("*        X = 修改成绩                *\n");
+	printf("*        E = 继续添加学生             *\n");
+	printf("*        Q = 退出系统                *\n");
 	printf("**************************************\n");
 	printf("*   Copyright <C> 2023.7.15 By lyj  *\n");
 	printf("**************************************\n");
 }
-void del() //删除学生信息
-{
-	int i, j, a;
-	int flag=0;
-	printf("请输入要删除的学生学号：");
-	scanf("%d", &a);
-	for(i = 0; i < n; i++)
-	{
-		if(st[i].num == a)
-		{
-			flag = 1;
-			for (j = i; j < n - 1; j++)
-			{
-				st[j] = st[j + 1];//直接把后面的学生都往前移动一位 
-			}
-		}
-		if (flag == 1) break;//说明已经找到了需要删除的学生，结束循环 
-	}
-	if (0 == flag)
-	{
-		printf("该学号不存在！！！\n");
-	}
-
-	if (1 == flag)
-	{
-		printf("删除成功\n");
-		n--;
-	}
-}
-
-void modify()
-{
-    int i, flag = 0;
-    char a[20]; // 学号应该是字符串类型
-    float score1;
-    printf("请输入要修改的学生学号：");
-    scanf("%s", a); // 使用%s读取学号
-
-    while (1)
-    {
-        flag = 0;
-        for (i = 0; i < n; i++)
-        {
-            if (st[i].num == atoi(a)) // 将学号字符串转换为整数进行比较
-            {
-                flag = 1;
-                int num; // 用于switch函数的子菜单
-                printf("***************************************************\n");
-                printf("*----1.修改语文--------  -  -----2.修改数学-------*\n");
-                printf("*----3.修改外语--------  -  -----4.退出本菜单--*\n");
-                printf("***************************************************\n\n");
-                printf("请输入子菜单：");
-                scanf("%d", &num);
-
-                switch (num)
-                {
-                case 1:
-                    printf("请输入新的语文成绩:\n");
-                    scanf("%f", &score1);
-                    st[i].score[0] = score1;
-                    break;
-                case 2:
-                    printf("请输入新的高数成绩:\n");
-                    scanf("%f", &score1);
-                    st[i].score[1] = score1;
-                    break;
-                case 3:
-                    printf("请输入新的英语成绩:\n");
-                    scanf("%f", &score1);
-                    st[i].score[2] = score1;
-                    break;
-                case 4:
-                    return; // 结束函数
-                default:
-                    printf("请在1--4之间选择！\n");
-                }
-                if (num > 0 && num < 4)
-                    printf("修改成功\n");
-
-                return; // 找到学生并修改成功后，结束函数
-            }
-        }
-        if (flag == 0)
-        {
-            printf("没有找到该学生，请重新输入需要修改信息的学生学号：\n");
-            // 使用 getchar 来清除输入缓冲区中的换行符
-            while (getchar() != '\n')
-                continue;
-            scanf("%s", a); // 使用%s读取学号
-        }
-    }
-}
-
 
 //*****成绩录入*******
-void type()
+void type(STU *head)
 {
 	int i;
 	printf("请输入学生人数：");
 	scanf("%d", &n);
-	printf("请输入%d名学生的三门课成绩：\n", n);
-	printf("学号	语文	数学	外语\n");
 
-	for(i = 1; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
-		scanf("%d	%f	%f	%f", &st[i-1].num, &st[i-1].score[0], &st[i-1].score[1], &st[i-1].score[2]);
+		STU *newStudent = (STU *)malloc(sizeof(STU));
+		if (newStudent == NULL)
+		{
+			printf("内存分配失败。\n");
+			exit(1);
+		}
+
+		printf("请输入第%d名学生的学号和三门课成绩：\n", i + 1);
+		scanf("%d %f %f %f", &newStudent->num, &newStudent->score[0], &newStudent->score[1], &newStudent->score[2]);
+
+		newStudent->next = head->next;
+		head->next = newStudent;
 	}
 }
-//*****列出成绩******
-void list()
+
+void list(STU *head)
 {
-	int i;
+	STU *current = head->next; // 跳过头结点，从第一个学生节点开始遍历
+
 	printf("学生成绩如下：\n");
-	printf("学号	语文	数学	外语	平均分\n");
-	for(i = 1; i <= n; i++)
+	printf("学号    语文    数学    外语    平均分\n");
+
+	while (current != NULL)
 	{
-		printf("%d	%.1f %.1f	%.1f	%.1f\n", st[i-1].num, st[i-1].score[0], st[i-1].score[1], st[i-1].score[2], st[i-1].ave);
+		printf("%d     %.1f     %.1f     %.1f     %.1f\n", current->num, current->score[0], current->score[1], current->score[2], current->ave);
+		current = current->next; // 移动到下一个学生节点
 	}
 }
+
 //*****求出平均值******
-void average()
+void average(STU *head)
 {
-	int sum = 0, i;
-	for(i = 0; i < n; i++)
+	STU *current = head->next;
+
+	while (current != NULL)
 	{
-		sum = st[i].score[0] + st[i].score[1] + st[i].score[2];
-		st[i].ave = sum / 3;
+		int sum = current->score[0] + current->score[1] + current->score[2];
+		current->ave = sum / 3.0;
+
+		current = current->next;
 	}
+
 	printf("平均分已计算。请使用命令L查看。\n");
 }
+
 //*****按平均分从高到低排序*****
-void sort()
+void sort(STU *head)
 {
-	int i, j;
-	STU tmp;
-	for(i = 0; i < n-1; i++)
+	int swapped;
+	STU *ptr1;
+	STU *lptr = NULL;
+
+	// 检查是否为空链表
+	if (head->next == NULL)
 	{
-		for(j = 0; j < n-i-1; j++)
-		{
-			if(st[j].ave < st[j+1].ave)
-			{
-				tmp = st[j];
-				st[j] = st[j+1];
-				st[j+1] = tmp;
-			}
-		}
+		printf("成绩表为空！\n");
+		return;
 	}
+
+	// 外层循环，控制排序迭代
+	while (lptr != head->next)
+	{
+		swapped = 0;
+		ptr1 = head->next;
+
+		// 内层循环，遍历链表并比较相邻节点的平均分
+		while (ptr1->next != lptr)
+		{
+			if (ptr1->ave < ptr1->next->ave)
+			{
+				// 交换节点的数据
+				STU tmp = *ptr1;
+				*ptr1 = *(ptr1->next);
+				*(ptr1->next) = tmp;
+
+				swapped = 1;
+			}
+			ptr1 = ptr1->next;
+		}
+
+		// 如果没有发生交换，表示已排序完成
+		if (!swapped)
+			break;
+
+		lptr = ptr1; // 更新已排序部分的末尾节点
+	}
+
 	printf("完成排序。请使用命令L查看\n");
 }
+
+void del(STU *head)
+{
+	int targetNum;
+	printf("请输入要删除的学生学号：");
+	scanf("%d", &targetNum);
+
+	STU *PDel = NULL;	  // 用于标记待删除的节点
+	STU *previous = head; // 指向当前节点的前一个节点
+
+	while (previous->next != NULL)
+	{
+		if (previous->next->num == targetNum)
+		{
+			PDel = previous->next;
+			previous->next = PDel->next; // 删除节点
+			free(PDel);					 // 释放节点内存
+			PDel = NULL;
+			printf("删除成功\n");
+			return;
+		}
+		else
+		{
+			previous = previous->next;
+		}
+	}
+
+	printf("未找到该学生学号\n");
+}
+
 //*****按学号查询******
 void search()
 {
-	int i, j;
+	int targetNum;
 	printf("请输入要查找的学生学号：");
-	scanf("%d", &i);
-	for(j = 0; j < n; j++)
+	scanf("%d", &targetNum);
+
+	// 遍历链表查找学号匹配的学生
+	STU *current = H->next;
+	int found = 0; // 标记是否找到匹配的学生
+
+	while (current != NULL)
 	{
-		if(st[j].num == i)
+		if (current->num == targetNum)
 		{
-			printf("学号	语文	数学	外语	平均分\n");
-			printf("%d	%.1f	%.1f	%.1f	%.1f\n", i, st[j].score[0], st[j].score[1], st[j].score[2], st[j].ave);
+			printf("学号\t语文\t数学\t外语\t平均分\n");
+			printf("%d\t%.1f\t%.1f\t%.1f\t%.1f\n", current->num, current->score[0], current->score[1], current->score[2], current->ave);
+			found = 1; // 找到匹配的学生
+			break;	   // 不需要继续遍历
 		}
+		current = current->next;
+	}
+
+	if (!found)
+	{
+		printf("未找到该学生学号\n");
 	}
 }
+
 //*****新增学生的成绩录入******
 void add()
 {
 	int addnum, i;
 	printf("请输入学生人数：");
 	scanf("%d", &addnum);
-	printf("请输入%d名学生的三门课成绩：\n", addnum);
-	printf("学号	语文	数学	外语\n");
-	n = n + addnum;
-	for(i = n - addnum; i < n; i++)
-	{
-		scanf("%d	%f	%f	%f", &st[i].num, &st[i].score[0], &st[i].score[1], &st[i].score[2]);
-	}
- 
 
+	// 循环添加新的学生节点
+	for (i = 0; i < addnum; i++)
+	{
+		STU *newStudent = (STU *)malloc(sizeof(STU));
+		if (newStudent == NULL)
+		{
+			printf("内存分配失败。\n");
+			exit(1);
+		}
+
+		printf("请输入第%d名学生的学号和三门课成绩：\n", i + 1);
+		scanf("%d %f %f %f", &newStudent->num, &newStudent->score[0], &newStudent->score[1], &newStudent->score[2]);
+
+		newStudent->next = H->next;
+		H->next = newStudent;
+		n++; // 更新学生数量
+	}
+
+	printf("新增学生信息完成。\n");
+}
+void modify()
+{
+	int targetNum;
+	printf("请输入要修改的学生学号：");
+	scanf("%d", &targetNum);
+
+	STU *current = H->next;
+	int found = 0; // 标记是否找到匹配的学生
+
+	while (current != NULL)
+	{
+		if (current->num == targetNum)
+		{
+			found = 1; // 找到匹配的学生
+
+			int num; // 用于switch函数的子菜单
+			printf("***************************************************\n");
+			printf("*----1.修改语文--------  -  -----2.修改数学-------*\n");
+			printf("*----3.修改外语--------  -  -----4.退出本菜单--*\n");
+			printf("***************************************************\n\n");
+			printf("请输入子菜单：");
+			scanf("%d", &num);
+
+			switch (num)
+			{
+			case 1:
+				printf("请输入新的语文成绩:\n");
+				scanf("%f", &(current->score[0]));
+				break;
+			case 2:
+				printf("请输入新的数学成绩:\n");
+				scanf("%f", &(current->score[1]));
+				break;
+			case 3:
+				printf("请输入新的外语成绩:\n");
+				scanf("%f", &(current->score[2]));
+				break;
+			case 4:
+				return; // 结束函数
+			default:
+				printf("请在1--4之间选择！\n");
+			}
+
+			printf("修改成功\n");
+			return; // 找到学生并修改成功后，结束函数
+		}
+		current = current->next;
+	}
+
+	if (!found)
+	{
+		printf("没有找到该学生，请重新输入需要修改信息的学生学号：\n");
+	}
 }
